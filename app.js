fb3a0d44da923f5433871c53038663b5e7d58a76
Jean-Noel-Esc <60701865+Jean-Noel-Esc@users.noreply.github.com>
2021-06-23 14:51:18 +0200

    basic display of tetrominoes on the grid
    function draw and undraw
    time and intervals
    function moveDown fall every second
    display a new tetro  when at the bottom

--- app.js
+++ app.js
@@ -44,16 +44,68 @@ const iTetromino = [
 const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
 let currentPosition = 4
-let current = theTetrominoes[0][0]
-console.log(theTetrominoes)
+let currentRotation = 0
 
-//draw the first rotation in the first tTetromino
+//randomly select a tetro and it's first rotation
+let random = Math.floor(Math.random()*theTetrominoes.length)
+let current = theTetrominoes[random][currentRotation]
+
+//draw the tetro
 function draw() {
     current.forEach(index => {
-        squares [(currentPosition + index)].classList.add('tetromino')
+        squares [currentPosition + index].classList.add('tetromino')
     })
 }
-draw();
 
 
-});
+
+//undraw the tetro
+function undraw(){
+    current.forEach(index =>{
+        squares[currentPosition + index].classList.remove('tetromino')
+    })
+}
+
+// TIME AND INTERVALS
+//make the tetro move down every second
+timerId = setInterval(moveDown, 1000)
+
+//Move down function
+function moveDown(){
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+} 
+
+//freeze function
+function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        //start the fall of new tetro
+        random = Math.floor(Math.random() * theTetrominoes.length)
+        current = theTetrominoes[random][currentRotation]
+        currentPosition = 4
+        draw()
+    }   
+}
+
+//move the tetro left unless is at the edge or there is a block
+
+function moveLeft(){
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition +index)% width === 0)
+    
+    if(!isAtLeftEdge) currentPosition -=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition +=1
+    }
+}
+
+
+
+
+
+
+})
